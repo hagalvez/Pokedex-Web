8ac4ba52a25bd08a9be207eb65eab258f36f9f9e
Hernán <hagalvez@outlook.com>
2023-08-24 17:19:45 -0300

    dgv con stored procedure

--- Poke-Asp/pokedex-web/PokemonsLista.aspx
+++ Poke-Asp/pokedex-web/PokemonsLista.aspx
@@ -5,5 +5,26 @@
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Lista de pokemons</h1>
+    <asp:GridView ID="dgvPokemons" runat="server" CssClass="table table-bordered text-center" AutoGenerateColumns="false">
+
+        <Columns>
+
+            <asp:BoundField HeaderText="Número" DataField="Numero" />
+            <asp:BoundField HeaderText="Nombre" DataField="Nombre" />
+            <asp:BoundField HeaderText="Descripción" DataField="Descripcion" />
+            <asp:BoundField HeaderText="UrlImagen" DataField="UrlImagen" />
+            <asp:BoundField HeaderText="Tipo" DataField="Tipo.Descripcion" />
+            <asp:BoundField HeaderText="Debilidad" DataField="Debilidad.Descripcion" />
+
+        </Columns>
+
+
+    </asp:GridView>
+
+
+
+
+
+
 
 </asp:Content>
--- negocio/PokemonNegocio.cs
+++ negocio/PokemonNegocio.cs
@@ -38,7 +38,7 @@ namespace negocio
 
                     //if(!(lector.IsDBNull(lector.GetOrdinal("UrlImagen"))))
                     //    aux.UrlImagen = (string)lector["UrlImagen"];
-                    if(!(lector["UrlImagen"] is DBNull))
+                    if (!(lector["UrlImagen"] is DBNull))
                         aux.UrlImagen = (string)lector["UrlImagen"];
 
                     aux.Tipo = new Elemento();
@@ -59,6 +59,48 @@ namespace negocio
                 throw ex;
             }
 
+        }
+
+        public List<Pokemon> listarConSP()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                //string consulta = "Select Numero, Nombre, P.Descripcion, UrlImagen, E.Descripcion Tipo, D.Descripcion Debilidad, P.IdTipo, P.IdDebilidad, P.Id From POKEMONS P, ELEMENTOS E, ELEMENTOS D Where E.Id = P.IdTipo And D.Id = P.IdDebilidad And P.Activo = 1";
+
+
+                //datos.setearConsulta(consulta);
+
+                datos.setearProcedimiento("storedListar");
+
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Numero = datos.Lector.GetInt32(0);
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    if (!(datos.Lector["UrlImagen"] is DBNull))
+                        aux.UrlImagen = (string)datos.Lector["UrlImagen"];
+
+                    aux.Tipo = new Elemento();
+                    aux.Tipo.Id = (int)datos.Lector["IdTipo"];
+                    aux.Tipo.Descripcion = (string)datos.Lector["Tipo"];
+                    aux.Debilidad = new Elemento();
+                    aux.Debilidad.Id = (int)datos.Lector["IdDebilidad"];
+                    aux.Debilidad.Descripcion = (string)datos.Lector["Debilidad"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public void agregar(Pokemon nuevo)
@@ -116,7 +158,7 @@ namespace negocio
             try
             {
                 string consulta = "Select Numero, Nombre, P.Descripcion, UrlImagen, E.Descripcion Tipo, D.Descripcion Debilidad, P.IdTipo, P.IdDebilidad, P.Id From POKEMONS P, ELEMENTOS E, ELEMENTOS D Where E.Id = P.IdTipo And D.Id = P.IdDebilidad And P.Activo = 1 And ";
-                if(campo == "Número")
+                if (campo == "Número")
                 {
                     switch (criterio)
                     {
@@ -131,7 +173,7 @@ namespace negocio
                             break;
                     }
                 }
-                else if(campo == "Nombre")
+                else if (campo == "Nombre")
                 {
                     switch (criterio)
                     {
@@ -198,7 +240,7 @@ namespace negocio
             {
                 AccesoDatos datos = new AccesoDatos();
                 datos.setearConsulta("delete from pokemons where id = @id");
-                datos.setearParametro("@id",id);
+                datos.setearParametro("@id", id);
                 datos.ejecutarAccion();
 
             }
