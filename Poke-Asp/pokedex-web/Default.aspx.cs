72d2f5d5892a29ec440067b40b9ceae1e7f85cca
Hernán <hagalvez@outlook.com>
2023-08-26 11:23:15 -0300

    Añadido Cards pokes

--- Poke-Asp/pokedex-web/Default.aspx.cs
+++ Poke-Asp/pokedex-web/Default.aspx.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using negocio;
 
 namespace pokedex_web
 {
     public partial class Default : System.Web.UI.Page
     {
+        // creo una property y la cargo con el resultado de ir a buscar a la base de datos, para luego usar esa lista en el front de mi default
+        public List<Pokemon> listaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listarConSP();
         }
-    }
+    } 
 }
---
+++ Poke-Asp/pokedex-web/Detalle.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Detalle.aspx.cs" Inherits="pokedex_web.Detalle" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Detalle pokemons ..    </h1>
+    <a href="Default.aspx">Volver</a>
+</asp:Content>
